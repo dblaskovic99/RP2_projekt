71b81d2a65679d16620b6f94e3b1451026db9c88
Ela Žuvanić <127298476+ElaZuva@users.noreply.github.com>
2023-06-27 10:31:57 +0200

    Add files via upload

---
+++ RP2_projekt/view/logged_inTrener.php
@@ -0,0 +1,9 @@
+<?php require_once __SITE_PATH . '/view/_headerTrener.php'; ?>
+
+<p>
+	Bravo, poštovani korisniče <?php echo $_SESSION['username']; ?>! Uspješno ste se ulogirali!
+</p>
+
+
+
+<?php require_once __SITE_PATH . '/view/_footer.php'; ?>
