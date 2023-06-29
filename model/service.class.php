5c64fcde05d9bee35b6729a1c2937d79a041e7d7
Ela Žuvanić <127298476+ElaZuva@users.noreply.github.com>
2023-06-30 00:54:08 +0200

    Add files via upload
    Popravila tablice s treninzima

--- model/service.class.php
+++ model/service.class.php
@@ -39,8 +39,6 @@ class Service{
 			$row['datum_rodenja'], $row['kategorija'], $row['id_trener'] ,
 			$row['id_klub'], $row['registration_sequence'], $row['has_registered']);
 
-
-    
 	}
 
 
@@ -60,6 +58,7 @@ class Service{
 		else
 			return new Trener( $row['id_trener'],$row['username'], $row['password_hash'], $row['ime'], $row['prezime'], $row['id_klub'], $row['registration_sequence'], $row['has_registered']);
 	}
+
     function getTrenerPoUsername( $username )
 	{
 		try
@@ -78,75 +77,98 @@ class Service{
 	}
 
 	function getTreningPoSportas($id_sportas)
-{
-    try {
-        $db = DB::getConnection();
-        $st = $db->prepare('SELECT * FROM trening WHERE id_sportas = :id_sportas');
-        $st->execute(array('id_sportas' => $id_sportas));
-    } catch (PDOException $e) {
-        exit('PDO error ' . $e->getMessage());
-    }
-
-    $arr = array();
-    while ($row = $st->fetch()) {
-        $arr[] = new Trening(
-            $row['id_trening'],
-            $row['id_sportas'],
-            $row['vrsta'],
-            $row['ime'],
-			$row['odraden'],
-            $row['interval1'],
-            $row['rez_interval1'],
-            $row['interval2'],
-            $row['rez_interval2'],
-            $row['interval3'],
-            $row['rez_interval3'],
-            $row['interval4'],
-            $row['rez_interval4'],
-            $row['interval5'],
-            $row['rez_interval5'],
-            $row['interval6'],
-            $row['rez_interval6'],
-            $row['interval7'],
-            $row['rez_interval7'],
-            $row['interval8'],
-            $row['rez_interval8'],
-            $row['interval9'],
-            $row['rez_interval9'],
-            $row['interval10'],
-            $row['rez_interval10']
-        );
-    }
-
-    return $arr;
-}
-
-
-		function dodajTreningTrener($id_sportas, $vrsta, $ime, $interval1, $interval2, $interval3, $interval4, $interval5, $interval6, $interval7, $interval8, $interval9, $interval10){
+	{
+		try {
 			$db = DB::getConnection();
-			try{
-				$st = $db->prepare('INSERT INTO trening (id_sportas, vrsta, ime, odraden, interval1, rez_interval1, interval2, interval3, interval4, interval5, interval6, interval7, interval8, interval9, interval10) VALUES '.
-									'(:id_sportas, :vrsta, :ime, 0, :interval1, 0, :interval2, :interval3, :interval4, :interval5, :interval6, :interval7, :interval8, :interval9, :interval10)');
-				$st->execute (array('id_sportas' => $id_sportas,
-									 'vrsta' => $vrsta, 
-									 'ime' => $ime, 
-									 'interval1' => $interval1, 
-									 'interval2' => $interval2, 
-									 'interval3' => $interval3, 
-									 'interval4' => $interval4, 
-									 'interval5' => $interval5, 
-									 'interval6' => $interval6, 
-									 'interval7' => $interval7, 
-									 'interval8' => $interval8, 
-									 'interval9' => $interval9, 
-									 'interval10' => $interval10));
-			}
-			catch( PDOException $e ){
-				echo 'Greska u Service.class.php!';
-				return 0;
-			}
-
+			$st = $db->prepare('SELECT * FROM trening WHERE id_sportas = :id_sportas AND odraden=1');
+			$st->execute(array('id_sportas' => $id_sportas));
+		} catch (PDOException $e) {
+			exit('PDO error ' . $e->getMessage());
 		}
+		$trening=$st->fetchAll();
+		return $trening;
+
+		
+	}
+	function getTreningPoID($id_trening)
+	{
+		try {
+			$db = DB::getConnection();
+			$st = $db->prepare('SELECT * FROM trening WHERE id_trening = :id_trening');
+			$st->execute(array('id_trening' => $id_trening));
+		} catch (PDOException $e) {
+			exit('PDO error ' . $e->getMessage());
+		}
+		$trening=$st->fetchAll();
+		return $trening;
+
+		
+	}
+	
+	function getNeodradeneTreninge($id_sportas){
+		try {
+			$db = DB::getConnection();
+			$st = $db->prepare('SELECT * FROM trening WHERE id_sportas = :id_sportas AND odraden=0');
+			$st->execute(array('id_sportas' => $id_sportas));
+		} catch (PDOException $e) {
+			exit('PDO error ' . $e->getMessage());
+		}
+		$trening=$st->fetchAll();
+		return $trening;
+
+
+
+}
+	function napraviTrening($id_trening, $rez_interval1, $rez_interval2, $rez_interval3, $rez_interval4, $rez_interval5, $rez_interval6, $rez_interval7, $rez_interval8, $rez_interval9, $rez_interval10){
+		$db = DB::getConnection();
+		try{
+			$st = $db->prepare('UPDATE trening SET odraden = 1, rez_interval1 = :rez_interval1, rez_interval2 = :rez_interval2, rez_interval3 = :rez_interval3, rez_interval4 = :rez_interval4, rez_interval5 = :rez_interval5, rez_interval6 = :rez_interval6, rez_interval7 = :rez_interval7, rez_interval8 = :rez_interval8, rez_interval9 = :rez_interval9, rez_interval10 = :rez_interval10 WHERE id_trening = :id_trening');
+			$st->execute (array('id_trening' => $id_trening,
+		
+									'rez_interval1' => $rez_interval1, 
+									'rez_interval2' => $rez_interval2, 
+									'rez_interval3' => $rez_interval3, 
+									'rez_interval4' => $rez_interval4, 
+									'rez_interval5' => $rez_interval5, 
+									'rez_interval6' => $rez_interval6, 
+									'rez_interval7' => $rez_interval7, 
+									'rez_interval8' => $rez_interval8, 
+									'rez_interval9' => $rez_interval9, 
+									'rez_interval10' => $rez_interval10));
+		}
+		catch( PDOException $e ){
+			echo 'Greska u Service.class.php!';
+			return 0;
+		}
+
+	}
+
+	function dodajTreningTrener($id_sportas, $vrsta, $ime, $interval1, $interval2, $interval3, $interval4, $interval5, $interval6, $interval7, $interval8, $interval9, $interval10){
+		$db = DB::getConnection();
+		try{
+			$st = $db->prepare('INSERT INTO trening (id_sportas, vrsta, ime, odraden, interval1, rez_interval1, interval2, interval3, interval4, interval5, interval6, interval7, interval8, interval9, interval10) VALUES '.
+								'(:id_sportas, :vrsta, :ime, 0, :interval1, 0, :interval2, :interval3, :interval4, :interval5, :interval6, :interval7, :interval8, :interval9, :interval10)');
+			$st->execute (array('id_sportas' => $id_sportas,
+									'vrsta' => $vrsta, 
+									'ime' => $ime, 
+									'interval1' => $interval1, 
+									'interval2' => $interval2, 
+									'interval3' => $interval3, 
+									'interval4' => $interval4, 
+									'interval5' => $interval5, 
+									'interval6' => $interval6, 
+									'interval7' => $interval7, 
+									'interval8' => $interval8, 
+									'interval9' => $interval9, 
+									'interval10' => $interval10));
+		}
+		catch( PDOException $e ){
+			echo 'Greska u Service.class.php!';
+			return 0;
+		}
+
+	}
+
 	function dodajNovogTrenera( $username, $password, $ime, $prezime, $id_klub, $reg_seq )
 	{
 		try
@@ -164,48 +186,7 @@ class Service{
 		}
 		catch( PDOException $e ) { exit( 'Greška u bazi: ' . $e->getMessage() ); }
 	}
-	function getNeodradeneTreninge($id_sportas){
-		try {
-			$db = DB::getConnection();
-			$st = $db->prepare('SELECT * FROM trening WHERE id_sportas = :id_sportas AND odraden=0');
-			$st->execute(array('id_sportas' => $id_sportas));
-		} catch (PDOException $e) {
-			exit('PDO error ' . $e->getMessage());
-		}
-	
-		$arr = array();
-		while ($row = $st->fetch()) {
-			$arr[] = new Trening(
-				$row['id_trening'],
-				$row['id_sportas'],
-				$row['vrsta'],
-				$row['ime'],
-				$row['odraden'],
-				$row['interval1'],
-				$row['rez_interval1'],
-				$row['interval2'],
-				$row['rez_interval2'],
-				$row['interval3'],
-				$row['rez_interval3'],
-				$row['interval4'],
-				$row['rez_interval4'],
-				$row['interval5'],
-				$row['rez_interval5'],
-				$row['interval6'],
-				$row['rez_interval6'],
-				$row['interval7'],
-				$row['rez_interval7'],
-				$row['interval8'],
-				$row['rez_interval8'],
-				$row['interval9'],
-				$row['rez_interval9'],
-				$row['interval10'],
-				$row['rez_interval10']
-			);
-	}    return $arr;
-
-
-}}
+	}
 
 
 ?>
