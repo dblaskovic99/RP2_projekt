15e22801cf3e91878c92cd6554265c68ce1c4b63
ninadobs <ninadobs.math@pmf.hr>
2023-07-10 11:49:49 +0200

    odbrojavanje radi!

--- view/natjecanjaSportas_index.php
+++ view/natjecanjaSportas_index.php
@@ -1,5 +1,10 @@
 <?php require_once __SITE_PATH . '/view/_headerSportas.php'; ?>
 
+    <div class="trening-info" id="odbrojavanje">
+        <h2>Odbrojavanje do prvog natjecanja</h2>
+        <div id="countdown"></div>
+    </div>
+
 	<div class="trening-info">
 		<h2>Buduća natjecanja</h2>
 		<table>
@@ -26,53 +31,52 @@
 		</table>
 		
 	</div>
-	
-	<div class="trening-info" id="odbojavanje">
-		<h2>Odbrojavanje do prvog natjecanja:</h2>
-
-	</div>
    
     <script>
-    document.addEventListener("DOMContentLoaded", function() {
-		// Dohvati datume natjecanja iz PHP-a i spremi ih u JavaScript polje
+   document.addEventListener("DOMContentLoaded", function() {
+    // Dohvati datume natjecanja iz PHP-a i spremi ih u JavaScript polje
     var natjecanja = <?php echo json_encode($natjecanjaList); ?>;
 
     // Pronađi najbliže natjecanje
     var now = new Date().getTime();
     var closestDate = Infinity;
-    var closestEvent
+    var closestEvent;
     for (var i = 0; i < natjecanja.length; i++) {
-    var natjecanjeDateTime = new Date(natjecanja[i][2] + " 15:00:00").getTime();
-    if (natjecanjeDateTime > now && natjecanjeDateTime < closestDate) {
-        closestDate = natjecanjeDateTime;
-    closestEvent = natjecanja[i][3];
+        var natjecanjeDateTime = new Date(natjecanja[i][2] + " 15:00:00").getTime();
+        if (natjecanjeDateTime > now && natjecanjeDateTime < closestDate) {
+            closestDate = natjecanjeDateTime;
+            closestEvent = natjecanja[i][3];
+        }
     }
-  }
 
     // Update the count down every 1 second
     var x = setInterval(function() {
-    // Get today's date and time
-    var now = new Date().getTime();
+        // Get today's date and time
+        var now = new Date().getTime();
 
-    // Find the distance between now and the closest date
-    var distance = closestDate - now;
+        // Find the distance between now and the closest date
+        var distance = closestDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="demo"
-    document.getElementById("odbrojavanje").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+       // Combine the event name and countdown into one string
+        var countdownText = closestEvent + " za: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-    document.getElementById("odbrojavanje").innerHTML = "EXPIRED";
-    }
-  }, 1000);
-    });
+        // Output the result in an element with id="countdown"
+        document.getElementById("countdown").innerHTML = countdownText;
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+});
+
 	</script>
 
 
